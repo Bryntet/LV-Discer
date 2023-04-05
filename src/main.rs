aca408b2427f654228e3af3aa6bfcd1f94c39bd7
Edvin Bryntesson <epost@edvinbryntesson.se>
2023-04-05 17:36:21 +0200

    nvm

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,13 @@ struct MyApp {
     allowed_to_close: bool,
     show_confirmation_dialog: bool,
     counter: i32,
+    my_string: String,
+    my_f32: f32,
+    my_boolean: bool,
+
 }
+
+
 
 impl MyApp {
     fn new(cc: &eframe::CreationContext<'_>) -> Self {
@@ -26,6 +32,9 @@ impl MyApp {
         // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
         // for e.g. egui::PaintCallback.
         let counter = 0;
+        let my_string = "Hello world!".to_string();
+        let my_f32: f32 = 0.0;
+        let my_boolean = false;
         Self::default()
     }
 }
@@ -39,41 +48,36 @@ impl eframe::App for MyApp {
 
     fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("Try to close the window");
-            ui_counter(ui, &mut self.counter);
-        });
+            ui.heading("Flip UP, Official VMix tool");
+            ui.label("This is a label");
+            ui.hyperlink("https://github.com/emilk/egui");
+            ui.text_edit_singleline(&mut self.my_string);
+            if ui.button("Click me").clicked() { }
+            ui.add(egui::Slider::new(&mut self.my_f32, 0.0..=100.0));
+            ui.add(egui::DragValue::new(&mut self.my_f32));
 
+            ui.checkbox(&mut self.my_boolean, "Checkbox");
+            if self.my_boolean {
+                ui_counter(ui, &mut self.counter);
+            }
+
+
+            ui.separator();
+
+            
+            ui.collapsing("Click to see what is hidden!", |ui| {
+                ui.label("Not much, as it turns out");
+            });
+
+        });
+        
         if self.show_confirmation_dialog {
             // Show confirmation dialog:
             egui::Window::new("Do you want to quit?")
                 .collapsible(false)
                 .resizable(false)
                 .show(ctx, |ui| {
-                    ui.label("This is a label");
-                    ui.hyperlink("https://github.com/emilk/egui");
-                    ui.text_edit_singleline(&mut my_string);
-                    if ui.button("Click me").clicked() { }
-                    ui.add(egui::Slider::new(&mut my_f32, 0.0..=100.0));
-                    ui.add(egui::DragValue::new(&mut my_f32));
-
-                    ui.checkbox(&mut my_boolean, "Checkbox");
-
-                    #[derive(PartialEq)]
-                    enum Enum { First, Second, Third }
-                    ui.horizontal(|ui| {
-                        ui.radio_value(&mut my_enum, Enum::First, "First");
-                        ui.radio_value(&mut my_enum, Enum::Second, "Second");
-                        ui.radio_value(&mut my_enum, Enum::Third, "Third");
-                    });
-
-                    ui.separator();
-
-                    ui.image(my_image, [640.0, 480.0]);
-
-                    ui.collapsing("Click to see what is hidden!", |ui| {
-                        ui.label("Not much, as it turns out");
-                    });
-
+                    
 
                     ui.horizontal(|ui| {
                         if ui.button("Cancel").clicked() {
@@ -90,6 +94,9 @@ impl eframe::App for MyApp {
     }
     
 }
+
+
+
 fn ui_counter(ui: &mut egui::Ui, counter: &mut i32) {
     // Put the buttons and label on the same row:
     ui.horizontal(|ui| {
