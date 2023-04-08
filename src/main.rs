d90e8981108e428b459a0fb757a55aac190ad520
Edvin Bryntesson <epost@edvinbryntesson.se>
2023-04-08 15:59:55 +0200

    final color fix?

--- src/main.rs
+++ src/main.rs
@@ -80,11 +80,11 @@ impl Player {
                     let select_colour = format!("&Input={}&SelectedName={}.Fill.Color", &self.input_id, format!("h{}p{}",self.hole+1,self.num));
                     let url = format!("http://{}:8088/api/?",self.consts.ip);
                     let result = &player.results[self.hole];
-                    println!("{}Function=SetText&Value={}{}", &url, &result.score, &selection);
+                    println!("{}Function=SetColor&Value={}&{}", &url, &result.get_score_colour(), &select_colour);
                     // Set score
                     reqwest::blocking::get(format!("{}Function=SetText&Value={}{}", &url, &result.score, &selection)).unwrap();
                     // Set colour
-                    reqwest::blocking::get(format!("{}Function=SetColor&Value={}&{}", &url, &result.get_score_colour(), &select_colour)).unwrap();
+                    reqwest::blocking::get(format!("{}Function=SetColor&Value={}{}", &url, &result.get_score_colour(), &select_colour)).unwrap();
                     // Show score
                     reqwest::blocking::get(format!("{}Function=SetTextVisibleOn{}", &url, &selection)).unwrap();
                 }
