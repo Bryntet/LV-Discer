24bd1f412895e7e806524cfc8bec0bc9c50824f7
Edvin Bryntesson <epost@edvinbryntesson.se>
2023-09-03 19:16:34 +0200

    cleanup

--- build-release.sh
+++ build-release.sh
@@ -4,6 +4,6 @@ rm -rf pkg
 rm -rf ../../modules/companion-module-levandevideo-vmix
 wasm-pack build --target nodejs --release -d built/rust_pkg
 rm node_modules/rust-wasm-test-edvin -rf & rm pkg.tgz & rm lv-vmix.zip
-fnm use 18 && yarn install --check-files && tsc --build tsconfig.js && yarn companion-module-build && cp -r pkg ../../modules/companion-module-levandevideo-vmix
+fnm use 18 && yarn install --check-files && tsc --build tsconfig.json && yarn companion-module-build && cp -r pkg ../../modules/companion-module-levandevideo-vmix
 7z a lv-vmix.zip ./pkg/*
 
--- js_src/build-config.cts
+++
@@ -1,3 +0,0 @@
-module.exports = {
-	extraFiles: ['../built/rust_pkg/rust_wasm_test_edvin_bg.wasm'],
-}
