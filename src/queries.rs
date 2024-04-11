66f5328b3ec4690a477d8b9d64a4662a0485ba4d
Edvin Bryntesson <epost@edvinbryntesson.se>
2024-04-11 17:54:12 +0200

    enumify readable score matching to make easier to follow

--- src/queries.rs
+++ src/queries.rs
@@ -70,6 +70,91 @@ pub struct SimpleResult {
     pub is_out_of_bounds: bool,
     pub is_outside_putt: bool,
 }
+
+enum ReadableScore {
+    Bogey(BogeyType),
+    Par,
+    Birdie,
+    Eagle,
+    Albatross,
+    Ace,
+}
+
+enum BogeyType {
+    Single,
+    Double,
+    Triple,
+    Ouch
+}
+impl BogeyType {
+    const fn new(score: u8) -> Self {
+        match score {
+            1 => Self::Single,
+            2 => Self::Double,
+            3 => Self::Triple,
+            _ => Self::Ouch
+        }
+    }
+}
+impl From<&SimpleResult> for ReadableScore {
+    fn from(res: &SimpleResult) -> Self {
+        Self::new(res.score as i8,res.hole.par.map(|p|p as i8))
+    }
+}
+
+impl ReadableScore {
+    const fn new(score: i8, par: Option<i8>) -> Self {
+        let actual_score = if let Some(par) = par {
+            score - par
+        } else {
+            score
+        };
+        match actual_score {
+            0 => Self::Par,
+            -1 => Self::Birdie,
+            -2 => Self::Eagle,
+            -3 if score == 1 => Self::Ace,
+            -3 => Self::Albatross,
+            ..=-3 => Self::Ace,
+            1.. => Self::Bogey(BogeyType::new(actual_score as u8))
+        }
+    } 
+    
+    const fn to_colour(&self) -> &'static str {
+        use ReadableScore::*;
+        match self {
+            Bogey(bogey_type) => {
+                match bogey_type {
+                    BogeyType::Triple | BogeyType::Ouch => "AB8E77FF",
+                    BogeyType::Double => "CA988DFF",
+                    BogeyType::Single => "EC928FFF",
+                }
+            },
+            Par => "7E8490FF",
+            Birdie => "A6F8BBFF",
+            Eagle => "6A8BE7FF",
+            Ace | Albatross => "DD6AC9FF"
+        }
+    }
+    
+    const fn to_mov(&self) -> &'static str {
+        use ReadableScore::*;
+        match self {
+            Bogey(bogey_type) => match bogey_type {
+                BogeyType::Ouch => "40 ouch.mov",
+                BogeyType::Triple => "30 3xBogey.mov",
+                BogeyType::Double => "20 2xBogey.mov",
+                BogeyType::Single => "10 bogey.mov"
+            },
+            Par => "04 par.mov",
+            Birdie => "03 birdie.mov",
+            Eagle => "02 eagle.mov",
+            Albatross => "01 albatross.mov",
+            Ace => "00 ace.mov"
+        }
+    }
+}
+
 impl SimpleResult {
     pub fn actual_score(&self) -> f64 {
         if let Some(par) = self.hole.par {
@@ -79,39 +164,17 @@ impl SimpleResult {
             self.score
         }
     }
-
-    pub fn get_score_colour(&self) -> &str {
-        match self.actual_score() as i64 {
-            4 => "AB8E77FF", // TODO FIX CORRECT COLOUR
-            3 => "AB8E77FF",
-            2 => "CA988DFF",
-            1 => "EC928FFF",
-            0 => "7E8490FF",
-            -1 => "A6F8BBFF",
-            -2 => "6A8BE7FF",
-            -3 => "DD6AC9FF",
-            _ => "AB8E77FF",
-        }
+    
+    fn readable_score(&self) -> ReadableScore {
+        self.into()
+    }
+    
+    pub fn get_score_colour(&self) -> &'static str {
+        self.readable_score().to_colour()
     }
 
-    pub fn get_mov(&self) -> &str {
-        match self.actual_score() as i64 {
-            4 => "40 ouch.mov",
-            3 => "30 3xBogey.mov",
-            2 => "20 2xBogey.mov",
-            1 => "10 bogey.mov",
-            0 => "04 par.mov",
-            -1 => "03 birdie.mov",
-            -2 => "02 eagle.mov",
-            -3 => {
-                if self.score == 1.0 {
-                    "00 ace.mov"
-                } else {
-                    "01 albatross.mov"
-                }
-            }
-            _ => "",
-        }
+    pub fn get_mov(&self) -> &'static str {
+        self.readable_score().to_mov()
     }
 }
 #[derive(cynic::QueryFragment, Debug, Clone)]
