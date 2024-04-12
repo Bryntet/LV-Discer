533689924360eb1f49e1f1335f443b192fb13562
Edvin Bryntesson <epost@edvinbryntesson.se>
2024-04-12 18:04:48 +0200

    fix things

--- scripts/build-release.sh
+++ scripts/build-release.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-
 rm -rf pkg
-rm -rf ../../modules/companion-module-levandevideo-vmix
+rm -rf ../../../modules/companion-module-levandevideo-vmix
 wasm-pack build --target nodejs --release -d built/rust_pkg
 rm node_modules/rust-wasm-test-edvin -rf & rm pkg.tgz & rm lv-vmix.zip
-fnm use 18 && yarn install --check-files && tsc --build tsconfig.json && yarn companion-module-build && cp -r pkg ../../modules/companion-module-levandevideo-vmix
+fnm use 18 && yarn install --check-files && tsc --build tsconfig.json && yarn companion-module-build && cp -r pkg ../../../modules/companion-module-levandevideo-vmix
 7z a lv-vmix.zip ./pkg/*
 
