988aaa30d33a09d8f7b0f0258fe127e13999c0ad
Edvin Bryntesson <github@edvinbryntesson.se>
2024-09-05 11:41:11 +0200

    Update mod.rs

--- rust_controller/src/api/mod.rs
+++ rust_controller/src/api/mod.rs
@@ -127,7 +127,7 @@ pub fn launch() -> Rocket<Build> {
 
     let conf = {
         #[cfg(windows)]
-        let ip = IpAddr::V4("10.171.121.129".parse().unwrap());
+        let ip = IpAddr::V4("10.170.121.242".parse().unwrap());
         #[cfg(not(windows))]
         let ip = IpAddr::V4("10.180.121.3".parse().unwrap());
         rocket::Config {
